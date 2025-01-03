9cab357bf92601218627a1860ba05dc7a433a884
Ajay-CKT <ajayckt917@gmail.com>
2025-01-03 21:23:41 +0530

    register router - partial done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,22 @@
+const User = require("../models/User");
+
 const authController = {
   register: async (request, response) => {
     try {
-      response.json({ message: "Hello World!" });
+      const { name, email, password } = request.body;
+      const existingUser = await User.findOne({ email });
+      if (existingUser)
+        return response
+          .status(400)
+          .json({ message: "User already registered" });
+      const newUser = new User({
+        name,
+        email,
+        password,
+      });
+      await newUser.save();
+      response.json({ message: "Registered succussfully!" });
+      console.log(request.body);
     } catch (error) {
       response.status(500).json({ message: error.message });
     }
