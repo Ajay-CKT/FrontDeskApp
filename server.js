c1dea81937c661f82da72d9f8ffa17368fe5452c
Ajay-CKT <ajayckt917@gmail.com>
2025-01-03 20:11:14 +0530

    splitting server.js into server.js and app.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const express = require("express");
+const app = require("./app");
 const mongoose = require("mongoose");
 const { MONGODBURI, PORT } = require("./utils/config");
 
-const app = express();
-
-app.get("/", (request, response) => response.json({ message: "Hello World!" }));
-
 mongoose
   .connect(MONGODBURI)
-  .then(() => console.log("Connected to Database..."))
+  .then(() => {
+    console.log("Connected to Database...");
+    app.listen(PORT, () => console.log(`Server running @ localhost:${PORT}`));
+  })
   .catch((error) => console.error("Error connecting to mongoDB...", error));
-
-app.listen(PORT, () => console.log(`Server running @ localhost:${PORT}`));
