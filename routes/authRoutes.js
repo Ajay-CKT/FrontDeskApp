839d30820147413bc9f598c9ea5f5fea5e58e11a
Ajay-CKT <ajayckt917@gmail.com>
2025-01-03 20:32:16 +0530

    Updated controllers setup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,6 @@ const authController = require("../controllers/authController");
 
 const authRouter = express.Router();
 
-authRouter.get("/register", authController.register);
+authRouter.post("/register", authController.register);
 
 module.exports = authRouter;
